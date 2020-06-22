4b2a1c9bd28aef0292b73abe8892e5544474629d
konatsup <konatsukomatsu@gmail.com>
2020-06-23 01:26:29 +0900

    startActivity消す

--- app/src/main/java/app/nickname/myoji/bughouse/ListActivity.kt
+++ app/src/main/java/app/nickname/myoji/bughouse/ListActivity.kt
@@ -21,7 +21,6 @@ class ListActivity : AppCompatActivity() {
             override fun onItemClick(position: Int) {
                 val intent = Intent(this@ListActivity, DetailActivity::class.java)
                 intent.putExtra("TASK_NAME", taskList[position].name)
-                startActivity(intent)
             }
         })
 
